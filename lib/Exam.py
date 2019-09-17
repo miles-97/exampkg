cd378fd99c6bc685a263f1883483373f7ef9925a
Miles Gaitan <milesagaitan@gmail.com>
2019-09-17 19:59:04 -0400

    Update

--- lib/Exam.py
+++ lib/Exam.py
@@ -9,6 +9,7 @@ class Exam(ProblemSet):
     points = []
 
     def __init__(self,name="",points=None,problem_set=None):
+        self.points = []
         super(Exam,self).__init__(name)
         if problem_set != None and points != None : 
             self.points = points.copy()
@@ -24,6 +25,7 @@ class Exam(ProblemSet):
         index = self.find_problem(problem)
         if super(Exam,self).remove_problem(problem) == 0:
             self.points.pop(index)
+            return 0
         else:
             return -1
 
@@ -31,6 +33,9 @@ class Exam(ProblemSet):
         if self.check_index(index):
             self.problems[index] = problem.copy()
             self.points[index] = point_val
+            return 0
+        else:
+            return -1
 
     def insert_problem(self,index,problem,point_val):
         if not(self.check_index(index)):
@@ -49,6 +54,7 @@ class Exam(ProblemSet):
         temp = self.points[ind1]
         self.points[ind1] = self.points[ind2]
         self.points[ind2] = temp
+        return 0
 
     def move_problem(self,org,dst):
         super(Exam,self).move_problem(org,dst)
@@ -60,63 +66,61 @@ class Exam(ProblemSet):
             if i != org:
                 temp.append(self.points[i])
         self.points = temp.copy()
+        return 0
 
     def pop_problem(self):
         self.problems.pop()
         self.points.pop()
 
-    def save(self,filename):
-        super(Exam,self).save(filename) #write problemset
+    def as_string(self):
+        lines = (super(Exam,self).as_string()).split("\n")
+        point_str = ' '.join([str(p) for p in self.points]) #get points as string
+        lines.insert(1, point_str)
+        return '\n'.join(lines)
+ 
+    # cheeky solution to get around python's lack of method overloading
+    # Exam init can only take in problemsets, super.copy() serves as an
+    # Exam -> ProblemSet conversion function. Letting a problemset be 
+    # passed to init. A bit wasteful since self is copied twice
+    def copy(self):
+        return Exam(self.name,self.points,super(Exam,self).copy())
 
-        try:
-            with open(filename,'r+') as f:
-                content = f.read() #copy problemset
-                f.seek(0,0)
-                for p in self.points:
-                    f.write("{} ".format(p)) #write points to SOF
-                f.write('\n' + content) #rewrite problemset
-        except FileNotFoundError:
-            print("\"{}\" does not exist",format(filename))
+    def save(self,filename):
+        with open(filename,'w') as f:
+            f.write(self.as_string())
 
     def load(self,filename):
-        lines = []
+        with open(filename,'r') as f:
+            lines = f.readlines()
 
-        try:
-            with open(filename,'r') as f:
-                lines = f.readlines() #copy problemset
-        except FileNotFoundError:
-            print("\"{}\" does not exist",format(filename))
-
-        lines = [s[:-1] for s in lines] #strip \n
-        str_points = lines[0].split(" ") #get points as string
-        points = []
-        for s in str_points: #reassigning to org arr fails bc last element is always ''
-            if s.isdigit():
-                points.append(int(s))
-        self.points = points
-        del lines[0] #first line is points so delete them before passing lines to super
-        super(Exam,self).load_lines(lines)
+        self.name = lines[0].rstrip()
+        temp_points = [ int(num) for num in lines[1].rstrip().split() ]
+        for i in range(2,len(lines)):
+            values = lines[i].split("|") #get | seperated values
+            if values[0] == "Problem":
+                p = Problem(values[1],values[2])
+            elif values[0] == "MathProblem":
+                p = MathProblem(values[1],values[2],values[3],values[4])
+            self.add_problem(p,temp_points[i-2])
 
     def consolidate(self,exam,*args):
         super(Exam,self).consolidate(exam,*args)
 
-        for i in range(exam.get_length()):
-            self.points.append(exam.get_points_of_problem(i))
+        self.points = exam.get_points().copy()
         for e in args:
-            for i in range(e.get_length()):
-                self.points.append(e.get_points_of_problem(i))
+            self.points += e.get_points().copy()
 
     def get_points_of_problem(self,index):
-        try:
+        if self.check_index(index):
             return self.points[index]
-        except IndexError:
-            print("Out of range")
+        else:
+            return -1
 
     def get_percent_of_problem(self,index):
-        try:
+        if self.check_index(index):
             return self.points[index] / sum(self.points)
-        except IndexError:
-            print("Out of range")
+        else:
+            return -1
 
     def get_percent_of_problem_rounded(self,index):
         return round(get_percent_of_problem(index),2)
@@ -141,8 +145,7 @@ class Exam(ProblemSet):
             "Will you say what you think?"]
 
         for q in questions:
-            self.add_problem(Problem(q,"Yes"),1)
-
+            self.add_problem(Problem(q,"Yes"),random.randint(1,7))
 
     def as_text_file(self,filename):
         with open(filename,'w') as f:
@@ -150,77 +153,6 @@ class Exam(ProblemSet):
             for i in range(self.get_length()):
                 f.write("{}.) {} ({})\n\n\n".format(i+1,self.get_problem(i).get_question(),self.points[i]))
 
-def fill_points(n):
-    l = []
-    for i in range(n):
-        l.append(random.randint(1,5))
-    return l
 
-def test():
-    ps = ProblemSet("Problem_Set")
-    ps.test_fill(10)
-    points = fill_points(10)
-
-    exam = Exam("Exam_Test",points,ps)
-
-    prob = Problem("Q_Test","Q_Ans")
-    exam.add_problem(prob,5)
-    points.append(5)
-
-    assert (exam.get_total_points() == sum(points)), "get_total_points()"
-    assert (exam.get_points() == points), "get_points()"
-
-    exam.save("exam_test")
-    load_exam = Exam()
-    load_exam.load("exam_test")
-   
-    assert(load_exam.as_string() == exam.as_string()),"load/save"
-    assert(load_exam.get_points() == exam.get_points()),"load/save"
-    assert(load_exam.get_percent_of_problem(0) == exam.get_percent_of_problem(0)),"get % prob"
-
-    load_exam.remove_problem(prob)
-    assert(load_exam.find_problem(prob) == -1), "remove_problem"
-    
-    a = load_exam.get_problem(0)
-    ap = load_exam.get_points_of_problem(0)
-    b = load_exam.get_problem(9)
-    bp = load_exam.get_points_of_problem(9)
-
-    load_exam.swap_problem(0,9)
-    assert(load_exam.get_problem(0).as_string() == b.as_string()),"swap"
-    assert(load_exam.get_points_of_problem(0) == bp),"swap"
-    assert(load_exam.get_problem(9).as_string() == a.as_string()),"swap"
-    assert(load_exam.get_points_of_problem(9) == ap),"swap"
-
-    c = Problem("Ins Prob","Ins Ans")
-    load_exam.insert_problem(5,c,3)
-    assert(load_exam.get_problem(5).as_string() == c.as_string()),"ins"
-    assert(load_exam.get_points_of_problem(5) == 3),"in"
-
-    d = load_exam.get_problem(1)
-    dp = load_exam.get_points_of_problem(1)
-    load_exam.move_problem(1,10)
-    assert(load_exam.get_problem(9).as_string() == d.as_string()),"move"
-    assert(load_exam.get_points_of_problem(9) == dp),"move"
-
-    ps1 = ProblemSet()
-    ps2 = ProblemSet()
-    ps3 = ProblemSet()
-    ps1.test_fill(7)
-    ps2.test_fill_quotes()
-    ps3.test_fill_math(7)
-    e1 = Exam("consolidate1",fill_points(7),ps1)
-    e2 = Exam("consolidate1",fill_points(7),ps2)
-    e3 = Exam("consolidate1",fill_points(7),ps3)
-    test_string = (e1.as_string()).rstrip() + (e2.as_string()).rstrip() + e3.as_string()
-    e1.consolidate(e2,e3)
-    assert(e1.get_problem(7).as_string() == e2.get_problem(0).as_string()),"consolidate"
-    assert(e1.get_problem(14).as_string() == e3.get_problem(0).as_string()),"consolidate"
-
-    text_exam = Exam("Meaning Test")
-    text_exam.test_fill_quotes()
-    text_exam.as_text_file("Meaning_Test.txt")
-
-#end of test()
-
-if __name__ == "__main__" : test()
+if __name__ == "__main__" : 
+    print("Testing moved to test/Test_Exam.py")
